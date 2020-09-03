3ec5a20d8dad76145ce7867987c5f0a675e21d37
Prabhjot Singh <prabhjotlamba2001@gmail.com>
2020-09-03 09:34:10 +0530

    Updating

--- employee_project/employee_register/views.py
+++ employee_project/employee_register/views.py
@@ -4,12 +4,13 @@ from .models import  Employee
 
 # Create your views here.
 
-
+#On the basis tf the request employee_list function change the current page employee register page to employee list page  
 def employee_list(request):
     context = {'employee_list': Employee.objects.all()}
     return render(request, "employee_register/employee_list.html", context)
 
-
+# employee_form check the details enter in the from is valid or not, If not error then it shows the error 
+# otherwise it stores the data in the database
 def employee_form(request, id=0):
     if request.method == "GET":
         if id == 0:
@@ -26,10 +27,10 @@ def employee_form(request, id=0):
             form = EmployeeForm(request.POST,instance= employee)
         if form.is_valid():
             form.save()
-        return redirect('/employee/list')
+        return redirect('/list')
 
-
+# To perform deletetion operations 
 def employee_delete(request,id):
     employee = Employee.objects.get(pk=id)
     employee.delete()
-    return redirect('/employee/list')
+    return redirect('/list')
